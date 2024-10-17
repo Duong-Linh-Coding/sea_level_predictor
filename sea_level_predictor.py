79fb6f6e2651b4120c68e325adadeb7d312636a3
Duong-Linh-Coding <167986901+Duong-Linh-Coding@users.noreply.github.com>
2024-10-17 11:57:34 +0700

    Update sea_level_predictor.py
    Edit comment "#"

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -7,9 +7,11 @@ def draw_plot():
     # Read data from file
     df=pd.read_csv("epa-sea-level.csv")
 
-    # Create scatter plot
+    # Binding data for variable/ax (x,y)
     x = df['Year']
     y = df['CSIRO Adjusted Sea Level']
+    
+    # Create fig & ax for draw plot
     fig, ax = plt.subplots()
     plt.scatter(x, y)
     
